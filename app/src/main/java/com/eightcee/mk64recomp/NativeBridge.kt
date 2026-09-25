b546270de471b30393da9d5dfebfb0cffb805db4
8cee <aycee0@gmail.com>
2026-09-25 17:03:42 +0200

    Align touch button ids with DK64 virtual pad

--- app/src/main/java/com/eightcee/mk64recomp/NativeBridge.kt
+++ app/src/main/java/com/eightcee/mk64recomp/NativeBridge.kt
@@ -3,11 +3,20 @@ package com.eightcee.mk64recomp
 import android.view.Surface
 
 object NativeBridge {
-    const val BUTTON_A = 1
-    const val BUTTON_B = 2
-    const val BUTTON_Z = 3
-    const val BUTTON_START = 4
-    const val BUTTON_R = 5
+    const val BUTTON_A = 0
+    const val BUTTON_B = 1
+    const val BUTTON_Z = 2
+    const val BUTTON_L = 3
+    const val BUTTON_R = 4
+    const val BUTTON_START = 5
+    const val BUTTON_C_UP = 6
+    const val BUTTON_C_DOWN = 7
+    const val BUTTON_C_LEFT = 8
+    const val BUTTON_C_RIGHT = 9
+    const val BUTTON_DPAD_UP = 10
+    const val BUTTON_DPAD_DOWN = 11
+    const val BUTTON_DPAD_LEFT = 12
+    const val BUTTON_DPAD_RIGHT = 13
 
     init {
         runCatching {
